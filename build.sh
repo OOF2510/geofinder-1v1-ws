5783bc9b156b0e7f99a09238dbef6c44432bf760
oof2510 <64423517+OOF2510@users.noreply.github.com>
2026-01-08 17:44:33 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
 set -e
 
+# Get first argument as version
+if [ -z "$1" ]; then
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+VERSION=${1}
+
+echo "Running gofmt and go mod tidy..."
 gofmt -w -s .
 go mod tidy
 
+echo "Building the project..."
 mkdir -p dist
-go build -o dist/geofinder-1v1-ws.x86_64 .
-echo "Build completed. Executable is located at dist/geofinder-1v1-ws.x86_64"
+go build -v -x -o dist/geofinder-1v1-ws-${VERSION}.x86_64 .
+echo "Build completed. Executable is located at dist/geofinder-1v1-ws-${VERSION}.x86_64"
